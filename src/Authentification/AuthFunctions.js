e37e32c7a3825dbacb9f40011fb8bb6c6d7ff928
Olivier Currie <olivier_currie@icloud.com>
2025-11-22 18:12:39 -0500

    fixed bug again

--- src/Authentification/AuthFunctions.js
+++ src/Authentification/AuthFunctions.js
@@ -1,9 +1,12 @@
 import { auth } from "../firebase"
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "firebase/auth"
+import { useNavigate } from "react-router-dom";
+
 
 export const signup = (email, password) => createUserWithEmailAndPassword(auth, email, password);
 export const login = (email, password) => signInWithEmailAndPassword(auth, email, password);
 export const logout = () => {
-    signOut(auth);
-    navigate("/home");
+  signOut(auth).then(() => {
+    window.location.href = "/"; 
+  });
 };
